b5ac4bb8f7d780493b47fbb35b0dbe836db2adb9
guildarfSoftware <68065345+guildarfSoftware@users.noreply.github.com>
2021-03-08 12:43:44 +0100

    refactor snake controller

--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -40,23 +40,7 @@ public class SnakeController : PhysicsEntity, IHitable, IStompable
     //Dead State
     float deathAnimationCountdown;
     float deathAnimationTime = 0.5f;
-    [SerializeField] Collider2D stompCollider;
 
-    void Start()
-    {
-        health = GetComponent<Health>();
-        _mainCollider = GetComponent<Collider2D>();
-        stateMachine = new StateMachine(StateNumber);
-        stateMachine.SetCallbacks(StWalking, WalkUpdate, null, WalkBegin, WalkEnd);
-        stateMachine.SetCallbacks(StAtacking, AttackUpdate, null, AttackBegin, null);
-        stateMachine.SetCallbacks(StHit, HitUpdate, null, HitBegin, null);
-        stateMachine.SetCallbacks(StDead, DeathUpdate, null, DeathBegin, null);
-        stateMachine.State = StWalking;
-        StartCoroutine(stateMachine.CoroutineHandler());
-        player = GameObject.FindGameObjectWithTag("Player");
-        IgnoreDrag = true;
-        IgnoreGravity = true;
-    }
     #region CrossState Methods
 
     public void Hit(float amount)
@@ -83,40 +67,35 @@ public class SnakeController : PhysicsEntity, IHitable, IStompable
     #region walk State
     void WalkBegin()
     {
-        Vector2 position = transform.position;
-        if (IsWalkable(position + Vector2.right * (_mainCollider.bounds.extents.x + colliderSizeMargin)))
+        Vector2 movingDirection = facingDirection == FacingRight ? Vector2.right : Vector2.left;
+
+        Speed.x = movingDirection.x * walkSpeed;
+
+    }
+
+    int WalkUpdate()
+    {
+        if (canAttackPlayer) return StAtacking;
+
+        if (stuck) return StWalking;
+        Vector2 movingDirection = Speed.normalized;
+        float snakeWidth = _mainCollider.bounds.extents.x;
+        snakeWidth -= colliderSizeMargin;
+        Vector2 currentTilePosition = MapCoordenates.GetTilePosition(transform.position - (Vector3)(snakeWidth * movingDirection));
+
+        if (IsWalkable(currentTilePosition + movingDirection))
+            return StWalking;
+
+
+        if (IsWalkable(currentTilePosition - movingDirection))
         {
-            facingDirection = FacingRight;
-            Speed.x = walkSpeed;
-        }
-        else if (IsWalkable(position + Vector2.left * (_mainCollider.bounds.extents.x + colliderSizeMargin)))
-        {
-            facingDirection = FacingLeft;
-            Speed.x = walkSpeed;
+            ChangeDirection();
         }
         else
         {
             stuck = true;
             Speed.x = 0;
         }
-    }
-    int WalkUpdate()
-    {
-        if (canAttackPlayer) return StAtacking;
-
-        if (stuck) return StWalking;
-
-        Vector2 position = transform.position;
-        if (!IsWalkable(position + Speed.normalized * (_mainCollider.bounds.extents.x + colliderSizeMargin)))
-        {
-            ChangeDirection();
-            if (!IsWalkable(position + Speed.normalized * (_mainCollider.bounds.extents.x + colliderSizeMargin)))
-            {
-                stuck = true;
-                Speed.x = 0;
-            }
-        }
-
 
         return StWalking;
     }
@@ -126,6 +105,13 @@ public class SnakeController : PhysicsEntity, IHitable, IStompable
         Speed.x = 0;
     }
 
+    bool IsWalkable(Vector2 position)
+    {
+        bool isObstructed = MapCoordenates.IsSolid(position);
+        bool hasFloor = MapCoordenates.IsSolid(position + Vector2.down);
+
+        return !isObstructed && hasFloor;
+    }
     #endregion
 
     #region attack State
@@ -150,10 +136,7 @@ public class SnakeController : PhysicsEntity, IHitable, IStompable
         attackAnimationTime = attackAnimationDuration;
 
         GetComponent<Animator>().SetTrigger("Attack");
-        Animator animator = GetComponent<Animator>();
-        AnimatorClipInfo[] info = animator.GetNextAnimatorClipInfo(0);
-        AnimatorClipInfo[] info2 = animator.GetCurrentAnimatorClipInfo(0);
-        AnimatorStateInfo info3 = animator.GetNextAnimatorStateInfo(0);
+        player.GetComponent<PlayerController>().TakeDamage(1, transform.position);
     }
 
 
@@ -172,7 +155,7 @@ public class SnakeController : PhysicsEntity, IHitable, IStompable
 
     private void HitBegin()
     {
-        print("startHitAnim");//startDeath animation
+        print("startHitAnim");// animation
         hitAnimationCountdown = hitAnimationTime;
     }
     #endregion
@@ -196,6 +179,23 @@ public class SnakeController : PhysicsEntity, IHitable, IStompable
 
 
     #endregion
+
+    #region Core Methods
+    void Start()
+    {
+        health = GetComponent<Health>();
+        _mainCollider = GetComponent<Collider2D>();
+        stateMachine = new StateMachine(StateNumber);
+        stateMachine.SetCallbacks(StWalking, WalkUpdate, null, WalkBegin, WalkEnd);
+        stateMachine.SetCallbacks(StAtacking, AttackUpdate, null, AttackBegin, null);
+        stateMachine.SetCallbacks(StHit, HitUpdate, null, HitBegin, null);
+        stateMachine.SetCallbacks(StDead, DeathUpdate, null, DeathBegin, null);
+        stateMachine.State = StWalking;
+        StartCoroutine(stateMachine.CoroutineHandler());
+        player = GameObject.FindGameObjectWithTag("Player");
+        IgnoreDrag = true;
+        IgnoreGravity = true;
+    }
 
     // Update is called once per frame
     new void Update()
@@ -221,40 +221,24 @@ public class SnakeController : PhysicsEntity, IHitable, IStompable
         Speed.x = -Speed.x;
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (health.IsDead) return;
+            canAttackPlayer = true;
+            playerRelativePosition = other.transform.position - transform.position;
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            if (other.otherCollider == _mainCollider)   //player hit enemy body
-            {
-                if(health.IsDead) return;
-                canAttackPlayer = true;
-                playerRelativePosition = other.transform.position - transform.position;
-            }
-            else if (other.otherCollider == stompCollider) //player hit stompable area
-            {
-                health.TakeDamage(float.MaxValue);
-                stateMachine.State = StDead;
-            }
+            canAttackPlayer = false;
         }
-    }
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            if (other.otherCollider == _mainCollider)   //player hit enemy body
-            {
-                canAttackPlayer = false;
-            }
-        }
-    }
 
-    bool IsWalkable(Vector2 position)
-    {
-        bool isObstructed = MapCoordenates.IsSolid(position);
-        bool hasFloor = MapCoordenates.IsSolid(position + Vector2.down);
-
-        return !isObstructed && hasFloor;
     }
+    #endregion
 
 }
