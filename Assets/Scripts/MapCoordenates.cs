2ae18c48bd1380f01fe6da1c72f7792b0e4ecdf4
guildarfSoftware <68065345+guildarfSoftware@users.noreply.github.com>
2021-03-12 12:56:01 +0100

    small refactor map coordenates

--- Assets/Scripts/MapCoordenates.cs
+++ Assets/Scripts/MapCoordenates.cs
@@ -3,20 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-public static class MapCoordenates 
+public static class MapCoordenates
 {
-    static public Tilemap tilemap{get; private set;}
-    static bool initialized;
+    static public Tilemap tilemap { get; private set; }
+    public static bool initialized;
     // Start is called before the first frame update
-    static void Initialize()
+    public static void Initialize()
     {
-        GameObject tileMapObject = GameObject.FindWithTag("Map");    
-        if(tileMapObject != null)
+        GameObject tileMapObject = GameObject.FindWithTag("Map");
+        if (tileMapObject != null)
         {
             tilemap = tileMapObject.GetComponent<Tilemap>();
-            if(tilemap == null) Debug.LogError("TileMap Object not containing TileMapComponent");
+            if (tilemap == null) Debug.LogError("TileMap Object not containing TileMapComponent");
         }
-        else{
+        else
+        {
             Debug.LogError("No TileMap Object found");
         }
         initialized = true;
@@ -24,43 +25,43 @@ public static class MapCoordenates
 
     static public Vector2Int WorldPosTocoordenate(Vector3 worldPos)
     {
-        if(!initialized) Initialize();
+        if (tilemap == null) Initialize();
         GridLayout gridLayout = tilemap.transform.parent.GetComponentInParent<GridLayout>();
-        return (Vector2Int) gridLayout.WorldToCell(worldPos);
+        return (Vector2Int)gridLayout.WorldToCell(worldPos);
     }
 
     static public bool IsSolid(Vector3 worldPos)
     {
         Tile tile = GetTile(worldPos);
-        
-        if(tile != null)
+
+        if (tile != null)
         {
             return true;
         }
 
         return false;
-        
+
     }
 
     public static Bounds? GetCellBounds(Vector3 worldPos)
     {
-        if (!initialized) Initialize();
+        if (tilemap == null) Initialize();
 
         Vector3 position = GetTilePosition(worldPos);
-        
-        return new Bounds(position,tilemap.cellSize); 
+
+        return new Bounds(position, tilemap.cellSize);
     }
-    
+
     static Tile GetTile(Vector3 worldPos)
     {
-        if (!initialized) Initialize();
+        if (tilemap == null) Initialize();
         Vector3Int coordenate = tilemap.WorldToCell(worldPos);
         return tilemap.GetTile(coordenate) as Tile;
     }
 
     internal static Vector2 GetTilePosition(Vector3 worldPos)
     {
-        if (!initialized) Initialize();
+        if (tilemap == null) Initialize();
 
         Vector3Int coordenate = tilemap.WorldToCell(worldPos);
 
