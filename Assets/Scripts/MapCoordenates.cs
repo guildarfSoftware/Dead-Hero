6b1079a7d1f8975168efee210c712f2dd7834400
guildarfSoftware <68065345+guildarfSoftware@users.noreply.github.com>
2021-04-14 11:30:38 +0200

    Change mapcoordenates implementation of IsSolid

--- Assets/Scripts/MapCoordenates.cs
+++ Assets/Scripts/MapCoordenates.cs
@@ -5,69 +5,71 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 public static class MapCoordenates
 {
-    static public Tilemap tilemap { get; private set; }
+    static public Grid grid { get; private set; }
     public static bool initialized;
+    private static string[] solidLayers = { "Obstacles" };
+
+    static Vector2 halfCellSize;
+
     // Start is called before the first frame update
     public static void Initialize()
     {
-        GameObject tileMapObject = GameObject.FindWithTag("Map");
-        if (tileMapObject != null)
+        grid = GameObject.FindObjectOfType<Grid>();
+        if (grid == null)
         {
-            tilemap = tileMapObject.GetComponent<Tilemap>();
-            if (tilemap == null) Debug.LogError("TileMap Object not containing TileMapComponent");
+            Debug.LogError("No Grid found in the level");
         }
         else
         {
-            Debug.LogError("No TileMap Object found");
+            halfCellSize = grid.cellSize * 0.5f;
+            initialized = true;
         }
-        initialized = true;
     }
 
     static public Vector2Int WorldPosTocoordenate(Vector3 worldPos)
     {
-        if (tilemap == null) Initialize();
-        GridLayout gridLayout = tilemap.transform.parent.GetComponentInParent<GridLayout>();
+        if (grid == null) Initialize();
+        GridLayout gridLayout = grid.transform.parent.GetComponentInParent<GridLayout>();
         return (Vector2Int)gridLayout.WorldToCell(worldPos);
     }
 
     static public bool IsSolid(Vector3 worldPos)
     {
-        Tile tile = GetTile(worldPos);
+        Vector3 tileCenterPosition = WorldToTileCenterPosition(worldPos);
 
-        if (tile != null)
-        {
-            return true;
-        }
+        RaycastHit2D hit2D = Physics2D.BoxCast(tileCenterPosition, halfCellSize, 0, Vector2.zero, 0, LayerMask.GetMask(solidLayers));
+        return hit2D.collider != null;
 
-        return false;
+    }
 
+    private static Vector3 WorldToTileCenterPosition(Vector3 worldPos)
+    {
+        if (grid == null) Initialize();
+        Vector3 returnPos = Vector3Int.FloorToInt(worldPos);
+
+        returnPos += (Vector3) halfCellSize;
+
+        return returnPos;
     }
 
     public static Bounds? GetCellBounds(Vector3 worldPos)
     {
-        if (tilemap == null) Initialize();
-        Vector3Int coordenate = tilemap.WorldToCell(worldPos);
-        Bounds bounds =tilemap.GetBoundsLocal(coordenate);
+        if (grid == null) Initialize();
+        Vector3Int coordenate = grid.WorldToCell(worldPos);
+        Bounds bounds = grid.GetBoundsLocal(coordenate);
         bounds.center += bounds.extents;
         return bounds;
 
         //return new Bounds(position, tilemap.cellSize);
     }
 
-    static Tile GetTile(Vector3 worldPos)
-    {
-        if (tilemap == null) Initialize();
-        Vector3Int coordenate = tilemap.WorldToCell(worldPos);
-        return tilemap.GetTile(coordenate) as Tile;
-    }
-
     internal static Vector2 GetTilePosition(Vector3 worldPos)
     {
-        if (tilemap == null) Initialize();
+        if (grid == null) Initialize();
 
-        Vector3Int coordenate = tilemap.WorldToCell(worldPos);
+        Vector3Int coordenate = grid.WorldToCell(worldPos);
 
-        Vector3 position = tilemap.GetCellCenterWorld(coordenate);
+        Vector3 position = grid.GetCellCenterWorld(coordenate);
 
         return position;
     }
